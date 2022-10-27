0e7963dfb43368c9a3c9e005f1cad111f7982606
temigeorgee <george.temidayo@gmail.com>
2022-10-27 17:19:00 +0100

    fixed deployment issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content="#F8FFF4" />
-          <link rel="apple-touch-icon" href="/img/pakam-logo.png" />
+          <link rel="apple-touch-icon" href="/favicon.ico" />
 
           <meta name="author" content="Pakam" />
           <meta
@@ -14,15 +14,15 @@ class MyDocument extends Document {
             content="Enabling people, planet, and profit."
           />
           <meta name="keywords" content="card, payment, bank, fintech" />
-          <meta property="og:image" content="/img/pakam-logo.png" />
+          <meta property="og:image" content="/favicon.ico" />
           <meta
             property="og:description"
             content="Enabling people, planet, and profit."
           />
           <meta property="og:title" content="Pakam" />
 
-          <link href="/img/pakam-logo.png" rel="icon" type="image/png" />
-          <link href="/img/pakam-logo.png" rel="apple-touch-icon" />
+          <link href="/favicon.ico" rel="icon" type="image/png" />
+          <link href="/favicon.ico" rel="apple-touch-icon" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
